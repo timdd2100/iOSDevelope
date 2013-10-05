89ff88cbd3ab71f327d6da0b8971c03826754679
Kao Chao-Ting <timkao@timMac.local>
2013-10-05 16:22:11 +0800

    test

--- CoinToss/timkaoViewController.m
+++ CoinToss/timkaoViewController.m
@@ -22,8 +22,35 @@
     
     result.text = coinLandedOnHeads?@"Heads" : @"Tails";
     
+    if (coinLandedOnHeads == userCalledHeads) {
+        status.text = @"correct";
+    }
+    else
+    {
+        status.text = @"wrong";
+    }
     
-}
+    CABasicAnimation *rotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    
+    rotation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    
+    rotation.fromValue = [NSNumber numberWithFloat:0.0f];
+    rotation.toValue = [NSNumber numberWithFloat:720 * M_PI / 180.0f];
+    rotation.duration = 2.0f;
+    
+    [status.layer addAnimation:rotation forKey:@"rotate"];
+    
+    
+    CABasicAnimation *fade = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    
+    fade.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    
+    fade.fromValue = [NSNumber numberWithFloat:0.0f];
+    fade.toValue = [NSNumber numberWithFloat:1.0f];
+    fade.duration = 3.5f;
+    
+    [status.layer addAnimation:fade forKey:@"fade"];
+    }
 - (void)viewDidLoad
 {
     [super viewDidLoad];
