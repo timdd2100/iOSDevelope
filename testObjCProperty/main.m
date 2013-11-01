64039bc4d2ee490ad226f9fbe3bacd1f3ea09733
Kao Chao-Ting <timkao@timMac.local>
2013-11-01 23:19:50 +0800

    Initial Commit

---
+++ testObjCProperty/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  testObjCProperty
+//
+//  Created by Kao Chao-Ting on 2013/11/1.
+//  Copyright (c) 2013年 tim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
